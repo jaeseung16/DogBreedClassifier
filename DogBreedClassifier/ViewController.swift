b5610c09bfcc26c7b363f11d19564dc18a3ecbf8
Jae-Seung Lee <jaeseung@gmail.com>
2018-06-13 20:22:25 -0400

    Initial Commit
    To Do
    1. Bounding box?
    2. Add CoreML
    3. Check the error message: [discovery] errors encountered while discovering extensions: Error Domain=PlugInKit Code=13 "query cancelled" UserInfo={NSLocalizedDescription=query cancelled}

--- DogBreedClassifier/ViewController.swift
+++ DogBreedClassifier/ViewController.swift
@@ -7,19 +7,115 @@
 //
 
 import UIKit
+import Vision
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var choosePhotoButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func presentAlert(_ title: String, error: NSError) {
+        // Always present alert on main thread.
+        DispatchQueue.main.async {
+            let alertController = UIAlertController(title: title,
+                                                    message: error.localizedDescription,
+                                                    preferredStyle: .alert)
+            let okAction = UIAlertAction(title: "OK",
+                                         style: .default) { _ in
+                                            // Do nothing -- simply dismiss alert.
+            }
+            alertController.addAction(okAction)
+            self.present(alertController, animated: true, completion: nil)
+        }
     }
-
-
+    
+    func performVisionRequest(image: CGImage, orientation: CGImagePropertyOrientation) {
+        var requests: [VNRequest] = []
+        
+        let faceDetectionRequest = VNDetectFaceRectanglesRequest { (request, error) in
+            if let error = error as NSError? {
+                self.presentAlert("Face Detection Error", error: error)
+                return
+            }
+            
+            guard let results = request.results as? [VNFaceObservation] else {
+                return
+            }
+            
+            if results.count > 0 {
+                for k in 1...results.count {
+                    print("face \(k)")
+                }
+            } else {
+                print("No faces.")
+            }
+            
+        }
+        
+        requests.append(faceDetectionRequest)
+        
+        let imageRequestHandler = VNImageRequestHandler(cgImage: image, orientation: orientation, options: [:])
+        
+        DispatchQueue.global(qos: .userInitiated).async {
+            do {
+                try imageRequestHandler.perform(requests)
+            } catch let error as NSError {
+                print("Failed to perform image request: \(error)")
+                self.presentAlert("Image Request Failed", error: error)
+                return
+            }
+        }
+    }
+    
+    @IBAction func choosePhoto(_ sender: UIButton) {
+        let imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
+        imagePicker.sourceType = .photoLibrary
+        imagePicker.allowsEditing = false
+        
+        let presentLibrary = UIAlertAction(title: "Photo Library", style: .default) { (_) in
+            imagePicker.sourceType = .photoLibrary
+            self.present(imagePicker, animated: true)
+        }
+        
+        let cancel = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        
+        let prompt = UIAlertController(title: "Choose a Photo", message: "Please choose a photo", preferredStyle: .actionSheet)
+        
+        prompt.addAction(presentLibrary)
+        prompt.addAction(cancel)
+        
+        present(prompt, animated: true, completion: nil)
+    }
+    
 }
 
+extension ViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        guard let originalImage = info[UIImagePickerControllerOriginalImage] as? UIImage else {
+            return
+        }
+        
+        imageView.image = originalImage
+        
+        let cgOrientation = CGImagePropertyOrientation(rawValue: UInt32(originalImage.imageOrientation.rawValue))
+        
+        guard let cgImage = originalImage.cgImage else {
+            return
+        }
+        
+        performVisionRequest(image: cgImage, orientation: cgOrientation!)
+        
+        dismiss(animated: true, completion: nil)
+    }
+}
