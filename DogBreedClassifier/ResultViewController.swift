5a6c33351cb27f3b53029a2d03e470623c3e3c56
Jae-Seung Lee <jaeseung@gmail.com>
2018-12-25 15:07:56 -0500

    feat: ResultViewController.swift
    Add generateCombinedImage(), imageSize(), and shiftedFrame(for:)

--- DogBreedClassifier/ResultViewController.swift
+++ DogBreedClassifier/ResultViewController.swift
@@ -35,11 +35,10 @@ class ResultViewController: UIViewController {
         dismiss(animated: true, completion: nil)
     }
 
-    
     @IBAction func presentActivityController(_ sender: UIBarButtonItem) {
+        let combinedImage = generateCombinedImage()
         
-        
-        let activityController = UIActivityViewController(activityItems: [dogImageView.image!], applicationActivities: nil)
+        let activityController = UIActivityViewController(activityItems: [combinedImage], applicationActivities: nil)
         
         activityController.completionWithItemsHandler = { (activityType: UIActivity.ActivityType?, completed: Bool, returnedItems: [Any]?, activityError: Error?) in
             guard completed else { return }
@@ -49,4 +48,43 @@ class ResultViewController: UIViewController {
         present(activityController, animated: true, completion: nil)
     }
     
+}
+
+// MARK: Methods to create a combined image
+extension ResultViewController {
+    func generateCombinedImage() -> UIImage {
+        // Image size to capture
+        let imageSizeForCapture = imageSize()
+        
+        // Shift the frame of the view for capture
+        let frameForCapture = shiftedFrame(for: imageSizeForCapture)
+        
+        // Render view to an image
+        UIGraphicsBeginImageContext(imageSizeForCapture)
+        view.drawHierarchy(in: frameForCapture, afterScreenUpdates: true)
+        let combinedImage: UIImage = UIGraphicsGetImageFromCurrentImageContext()!
+        UIGraphicsEndImageContext()
+        
+        return combinedImage
+    }
+    
+    func imageSize() -> CGSize {
+        let imageSize = dogImageView.image!.size
+        
+        let ratioWidth = dogImageView.frame.size.width / imageSize.width
+        let ratioHeight = dogImageView.frame.size.height / imageSize.height
+        
+        let scale = ratioWidth < ratioHeight ? ratioWidth : ratioHeight
+        
+        return CGSize(width: imageSize.width * scale, height: imageSize.height * scale)
+    }
+    
+    func shiftedFrame(for imageSize: CGSize) -> CGRect {
+        let xOrigin = self.view.frame.origin.x - (self.view.frame.size.width - imageSize.width) * 0.5
+        let yOrigin = self.view.frame.origin.y - dogImageView.frame.origin.y - (dogImageView.frame.size.height - imageSize.height) * 0.5
+        
+        let shiftOrigin = CGPoint(x: xOrigin, y: yOrigin)
+        
+        return CGRect(origin: shiftOrigin, size: self.view.frame.size)
+    }
 }
